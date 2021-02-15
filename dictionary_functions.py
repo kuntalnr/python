130b6b0c259d619b1701dd10aa89b89deebb345e
Kuntal <kuntalnr@gmail.com>
2021-02-15 00:42:03 +0000

    day 1 of python tutorial

---
+++ dictionary_functions.py
@@ -0,0 +1,24 @@
+# dictionary is a key value pair
+# d1 = {} # an empty dictionary
+# print(type(d1))
+d2 = {"kuntal": "burger", "Anjana":"mutton","bhaiya":"chicken",
+      "shubham":{"B": "maggie", "L":"roti","D":"daal"}} # creates a dictionary within a dictionary
+# print(d2["kuntal"])
+# print(d2["shubham"])
+# print(d2["shubham"]["B"])
+# d2["Ankit"] = "junk food" # adds this to existing
+# d2["Aurangzeb"] = "kebabs"
+# del d2["Aurangzeb"] # deletes d2
+# print(d2)
+#print(d2.copy()) # copies the dictionary
+# d3 = d2
+# del d3["kuntal"]
+# print(d2) # this refernce d2 only. that is why use copy
+# d3 = d2.copy()
+# del d3["Anjana"]
+# print(d2)
+# print(d3)
+# d2.update({"Leena":"toffee"})
+# print(d2)
+# print(d2.keys())
+# print(d2.items())
---
+++ question1.py
@@ -0,0 +1,9 @@
+d2 = {"Immutable": "cannot be changed",
+      "Mutable": "can be changed",
+      "git": "version control system",
+      "github": "online repository for git"}
+
+print("enter a word")
+inputword = input()
+
+print(d2[inputword])
